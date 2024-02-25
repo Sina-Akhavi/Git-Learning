27aa35f0647a4aca3afc64b66f7efeea7f20d169
Sina Akhavi <SinaAkhaviSpecial1984@gmail.com>
2024-02-25 18:02:42 +0330

    finished till section reshaping

--- cv-lab1/lab1.py
+++ cv-lab1/lab1.py
@@ -99,4 +99,88 @@ A = np.array([[1, 2, 3, 5], [2, 5, 6, 5], [3, 3, 3, 5]])
 # print("mul=", A @ B)
 
 # ------------------------ creating random matrices ------------------------
-print(22 * np.random.random((2, 3)))
+# print(np.random.random((2, 3)), "\n\n")
+#
+# print(np.random.rand(2, 3))
+
+
+# ------------------------ Numpy slices ------------------------
+# A = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+# b = A.copy() # creates a copy of A
+#
+# b[0] = 9
+# print("A=", A)
+
+# ------------------------------- Mask -------------------------------
+# A = np.array([[1,2,3,4],
+#               [5,6,7,8],
+#               [9,10,11,12]])
+
+# Mask = np.array([[True, False, True, False],
+#                  [True, True, False, False],
+#                  [False, False, False, True]])
+#
+# print("masked A=", A[Mask])
+#
+#
+# A[Mask] = 222
+#
+# print("A=\n", A)
+
+# mask = A % 2 == 0
+# A[mask] = -1
+# print("A=", A)
+
+# ------------------------ Operations on arrays ------------------------
+# A = np.array([[1, 3, 4, 5],
+#               [11, 3, 4, 5],
+#              [1, 3, 4, 15]])
+#
+# print("A.max()=", A.max()) # maximum elements
+# print("A.max(axis=0)=", A.max(axis=0)) # maximum elements in columns
+# print("A.max(axis=1)=", A.max(axis=1)) # maximum elements in rows
+# print("A.max(axis=1, keepdims=True)=", A.max(axis=1, keepdims=True)) # maximum elements in columns
+#
+# print("A.mean(axis=1)=", A.mean(axis=1)) # takes the average of each row
+
+# ------------------------- concatenations -------------------------
+X = np.array([[1,2],[3,4]])
+Y = np.array([[10,20,30],[40,50,60]])
+Z = np.array([[7,7],[8,8],[9,9]])
+
+# A = np.concatenate((X,Z))
+# print("A=", A)
+# A = np.concatenate((X,Z), axis=0)
+# print("A=", A)
+#
+# A = np.concatenate((X,Y), axis=1)
+# print("A=", A)
+
+# A = np.vstack((X,Z))
+# print("A=", A)
+
+# A = np.hstack((X,Y))
+# print("A=", A)
+
+
+# A = np.tile(Y,(4,3))
+# print("A=", A)
+
+# ------------------------------- Reshaping -------------------------------
+# A = np.array([[1, 2],[3, 5]])
+# (A_reshaped = A.reshape(4, 3)) === (A.shape = (4, 3))
+
+
+# print("A_reshaped=", A)
+# print("A=", A)
+
+# ----------------------- ravel vs flatten -----------------------
+A = np.array([[1, 2], [3, 5], [9, 9]])
+
+a1 = A.flatten() # altering a1 does not affect A
+a1[1] = -13
+print("A=\n", A)
+
+a2 = A.ravel() # altering a2 does affect A
+a2[1] = -13
+print("A=\n", A)
