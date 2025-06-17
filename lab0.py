ef805fc089be7aa61958424ba7a4a99427218ab9
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-06-17 16:29:33 +0330

    add ------

--- lab0.py
+++ lab0.py
@@ -1,23 +1,3 @@
-# i = 4
-# print(type(i))
-
-# float_i = 4.0
-# print(type(float_i))
-
-# l = [10, 20, 30, 40.2, 'salam']
-# for k in l:
-#     print(k)
-
-# while 2 > 1:
-#     print("Hi")
-
-# ----------------- lambda function -----------------
-# f = lambda x, y: x + y
-#
-# output = f(1, 2)
-# print(output)
-
-# ---------------- task1: matrix multiplication ----------------
 def mul(A, B):
     m1 = len(A)
     n1 = len(A[0])
@@ -53,3 +33,5 @@ C = [[1, 0, 0],
 print(mul(A, B))
 print(mul(B,A))  # m x n    a * q
 print(mul(A,C))
+
+# ----------------------------------
